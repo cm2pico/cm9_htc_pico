6d4eb81db03b911e48c22846b32805bf0beac7ca
sakindia123 <sa6915@gmail.com>
2012-08-28 10:45:01 +0530

    Clean cm.mk

--- cm.mk
+++ cm.mk
@@ -7,9 +7,6 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Include GSM stuff
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-# Inherit from those products. Most specific first
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
@@ -27,7 +24,7 @@ PRODUCT_BRAND := htc_europe
 PRODUCT_DEVICE := pico
 PRODUCT_MODEL := HTC Explorer A310
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pico BUILD_ID=IMLK74 BUILD_FINGERPRINT=htc_asia_india/htc_pico/pico:2.3.5/GRJ90/171430.1:user/release-keys PRIVATE_BUILD_DESC="1.12.720.1 CL171430 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pico BUILD_ID=IMM76L BUILD_FINGERPRINT=htc_asia_india/htc_pico/pico:2.3.5/GRJ90/171430.1:user/release-keys PRIVATE_BUILD_DESC="1.12.720.1 CL171430 release-keys"
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := pico
